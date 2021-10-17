5a3f95e31af75a1e3e7f8ea4271cc4f706820de0
thisisbipin <bipinmjadav087@gmail.com>
2021-10-17 13:06:51 +0530

    Added Particles feature

---
+++ public/scripts/particleEffects.js
@@ -0,0 +1,6 @@
+
+function loadParticles() {
+    particlesJS.load('particles-js', 'public/assets/particles.json', function () {
+        console.log('callback - particles.js config loaded');
+    });
+}
